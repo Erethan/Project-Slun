f183a9b32d085333aeafee64a29957c0774433f8
Eric Marchetti <ericmsbr@gmail.com>
2020-04-20 18:54:50 -0300

    The capsule walks!

---
+++ Project Slun/Assets/Scripts/Player/AvatarMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(GroundCheck))]
+public class AvatarMovement : MonoBehaviour
+{
+    public FloatingJoystick joystick;
+    public float targetSpeed;
+    public float impulseForce;
+
+    private Rigidbody rigid;
+    private GroundCheck groundCheck;
+    public Vector3 Velocity { get; private set; }
+
+    [Header("Drag")]
+    public float walkingDrag;
+    public float stopedDrag;
+    public float airborneDrag;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        groundCheck = GetComponent<GroundCheck>();
+        Velocity = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        if (Velocity.sqrMagnitude > 0.1f)
+        {
+            transform.forward = Vector3.ProjectOnPlane(Velocity, Vector3.up);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Velocity = rigid.velocity;
+
+        //Add movement impulse
+        Vector3 inputAxis = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        if (Mathf.Abs(inputAxis.x) > float.Epsilon || Mathf.Abs(inputAxis.y) > float.Epsilon)
+        {
+            Vector3 planeDir = Vector3.ProjectOnPlane(inputAxis, groundCheck.GroundNormal);
+            planeDir.Normalize();
+
+            if(Velocity.sqrMagnitude < targetSpeed * targetSpeed)
+            {
+                rigid.AddForce(planeDir * impulseForce * inputAxis.magnitude, ForceMode.Impulse);
+            }
+        }
+
+        //Update drag
+        if (!groundCheck.Grounded)
+        {
+            rigid.drag = airborneDrag;
+        }
+        else if(inputAxis.sqrMagnitude > 0.1f)
+        {
+            rigid.drag = walkingDrag;
+        }
+        else
+        {
+            rigid.drag = stopedDrag;
+        }
+    }
+       
+}
---
+++ Project Slun/Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public CapsuleCollider capsuleCollider;
+    [Range(0, 0.1f)] public float groundCheckTolerance = 0.01f;
+    [Range(0, 1)] public float sphereCastProportion = 1;
+
+    public bool Grounded{get; private set;}
+    public Vector3 GroundNormal { get; private set; }
+
+
+    private void FixedUpdate()
+    {
+        RaycastHit hitInfo;
+        if (Physics.SphereCast(transform.position, capsuleCollider.radius * sphereCastProportion, Vector3.down,
+                out hitInfo, (capsuleCollider.height / 2f) + groundCheckTolerance,
+                ~0, QueryTriggerInteraction.Ignore))
+        {
+            GroundNormal = hitInfo.normal;
+            Grounded = true;
+        }
+        else
+        {
+            GroundNormal = Vector3.up;
+            Grounded = false;
+        }
+    }
+    
+}
