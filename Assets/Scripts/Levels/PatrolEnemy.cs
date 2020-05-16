645a5f77a05706078b4d60d4ee2b71295a3fde65
Eric Marchetti <ericmsbr@gmail.com>
2020-05-16 18:52:23 -0300

    First iteration on editor tools for Flying enemies

--- Assets/Scripts/Levels/PatrolEnemy.cs
+++ Assets/Scripts/Levels/PatrolEnemy.cs
@@ -7,68 +7,134 @@ using UnityEditor;
 
 public class PatrolEnemy : MonoBehaviour
 {
-    public Vector3 targetOffset;
     public GroundMovement movement;
+    //public Vector3 TargetPosition { get { return initialPosition + targetOffset; } }
 
-    private Vector3[] targetWorldPositions;
-    private int nextTargetIndex;
+    [SerializeField]  private Vector3 targetOffset = default;
+    [SerializeField] [HideInInspector] private Vector3 initialPosition = default;
+    [SerializeField] [HideInInspector] private int nextTargetIndex = 1;
 
+    public Vector3 TargetPosition { get { return initialPosition + targetOffset; } }
+
+    private Vector3 NextTarget
+    {
+        get
+        {
+            switch (nextTargetIndex)
+            {
+                case 0:
+                    return initialPosition;
+                default:
+                    return TargetPosition;
+            }
+        }
+    }
 
     private void Awake()
     {
-        targetWorldPositions = new Vector3[1 + 1]; // start + targets position TODO: make targetOffset an array
-        targetWorldPositions[0] = transform.position;
-        targetWorldPositions[1] = transform.position + targetOffset;
-        nextTargetIndex = 1;
+        initialPosition = transform.position;
     }
 
     private void Update()
     {
-        if( Vector3.Distance(transform.position, targetWorldPositions[nextTargetIndex]) < 0.25f )
+        if( Vector3.Distance(transform.position, NextTarget) < 0.25f )
         {
-            nextTargetIndex++;
-            if(nextTargetIndex == targetWorldPositions.Length)
-            {
-                nextTargetIndex = 0;
-            }
+            ChangeTarget();
             movement.ResetVelocity();
         }
-        movement.SetDirection(targetWorldPositions[nextTargetIndex] - transform.position);
+        movement.SetDirection(NextTarget - transform.position);
+    }
+
+    private void ChangeTarget()
+    {
+        nextTargetIndex++;
+        if (nextTargetIndex > 1)
+            nextTargetIndex = 0;
     }
     
-    public Vector3 GetNextTarget()
-    {
-        if(targetWorldPositions == null || targetWorldPositions.Length == 0)
-        {
-            return transform.position;
-        }
-
-        return targetWorldPositions [nextTargetIndex];
-    }
 }
 
 #if UNITY_EDITOR
-
+[CanEditMultipleObjects]
 [CustomEditor(typeof(PatrolEnemy))]
 public class PatrolEnemyEditor : Editor
 {
-    private Transform enemyTf;
+    private PatrolEnemy[] selectedPatrolEnemies;
+    private SerializedProperty[] initialPositionProps;
+    private SerializedProperty[] targetOffsetProps;
 
-    protected virtual void OnSceneGUI()
+    void OnEnable()
     {
-        if (Event.current.type == EventType.Repaint)
-        {
-            Vector3 targetPos = ((PatrolEnemy)target).GetNextTarget();
-            if(enemyTf == null) enemyTf = ((PatrolEnemy)target).transform;
+        Object[] inspectedObjects = serializedObject.targetObjects;
+        selectedPatrolEnemies = new PatrolEnemy[inspectedObjects.Length];
+        initialPositionProps = new SerializedProperty[inspectedObjects.Length];
+        targetOffsetProps = new SerializedProperty[inspectedObjects.Length];
 
-            if (Vector3.Distance(targetPos, enemyTf.position) < 0.1f)
+        for (int i = 0; i < inspectedObjects.Length; i++)
+        {
+            selectedPatrolEnemies[i] = targets[i] as PatrolEnemy;
+
+            SerializedObject so = new SerializedObject(inspectedObjects[i]);
+            initialPositionProps[i] = so.FindProperty("initialPosition");
+            targetOffsetProps[i] = so.FindProperty("targetOffset");
+
+        }
+
+    }
+
+
+    void OnSceneGUI()
+    {
+        if (!EditorApplication.isPlaying)
+        {
+            for (int i = 0; i < initialPositionProps.Length; i++)
             {
-                return;
+                initialPositionProps[i].vector3Value = selectedPatrolEnemies[i].transform.position;
+            }
+        }
+
+        if(!Event.current.control)
+        {
+            Vector3 centerHandlePos = Vector3.zero;
+            Vector3 previousCenterHandlePos;
+            for (int i = 0; i < selectedPatrolEnemies.Length; i++)
+            {
+                centerHandlePos += selectedPatrolEnemies[i].TargetPosition;
+                
             }
 
-            Handles.SphereHandleCap(-1, targetPos, Quaternion.identity, 0.1f, EventType.Repaint);
-            Handles.DrawAAPolyLine(6, enemyTf.position, targetPos);
+            centerHandlePos = centerHandlePos / selectedPatrolEnemies.Length;
+            previousCenterHandlePos = centerHandlePos;
+            centerHandlePos = Handles.PositionHandle(centerHandlePos, Quaternion.identity);
+
+            for (int i = 0; i < initialPositionProps.Length; i++)
+            {
+                targetOffsetProps[i].vector3Value += centerHandlePos - previousCenterHandlePos;
+            }
         }
+
+        for (int i = 0; i < initialPositionProps.Length; i++)
+        {
+
+            if (Event.current.control)
+            {
+                targetOffsetProps[i].vector3Value = Handles.PositionHandle(
+                    selectedPatrolEnemies[i].TargetPosition,
+                    Quaternion.identity) - initialPositionProps[i].vector3Value;
+            }
+
+            if (Event.current.type == EventType.Repaint)
+            {
+                Handles.SphereHandleCap(-1, selectedPatrolEnemies[i].TargetPosition, Quaternion.identity, 0.25f, EventType.Repaint);
+                Handles.DrawAAPolyLine(2, initialPositionProps[i].vector3Value, selectedPatrolEnemies[i].TargetPosition);
+            }
+
+            
+                targetOffsetProps[i].vector3Value = new Vector3(targetOffsetProps[i].vector3Value.x, 0, targetOffsetProps[i].vector3Value.z);
+            targetOffsetProps[i].serializedObject.ApplyModifiedProperties();
+        }
+
     }
 }
+
 #endif
